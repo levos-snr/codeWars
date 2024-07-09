c221da6118000f1ed9d1e8f94ff9d1e6392a23d6
Lewis Odero <lewisodero27@gmail.com>
2024-07-09 17:21:09 +0300

    fix

--- likes.js
+++ likes.js
@@ -9,15 +9,11 @@
 // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
-let names = []
-
 function likes(names) {
-
-
 
     if (names.length === 0) {
         return 'no one likes this';
-    } 
+    }
     else if(names.length === 1){
         return `${names[0]} likes this`;
     } else if (names.length === 2) {
@@ -25,10 +21,8 @@ function likes(names) {
     } else if(names.length === 3) {
         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     } else {
-        return `${names[0]}, ${names[1]}, ${names[2]} and ${names[3]} others like this`;
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this `
     }
 }
-
-console.log(likes(["Alex", "Jacob", "Mark", "Max"])); 
 
 
